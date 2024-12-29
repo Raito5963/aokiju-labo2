653ee7b7ddc8ccbb8996d9b790c3474f7436e766
Raito5963 <181489458+Raito5963@users.noreply.github.com>
2024-12-29 03:14:16 +0000

    初期設定と基本的なページコンポーネントの追加

---
+++ aokiju/app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Box, CssBaseline, Toolbar } from "@mui/material";
+
+import NavigationBar from "./_components/navigationbar";
+import SideBar from "./_components/sidebar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Aokiju Labo",
+  description: "Aokiju Labo top page",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <AppRouterCacheProvider>
+          <Box sx={{ display: "flex" }}>
+            <CssBaseline />
+            <NavigationBar />
+            <SideBar />
+            <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+              <Toolbar />
+              {children}
+            </Box>
+          </Box>
+        </AppRouterCacheProvider>
+      </body>
+    </html>
+  );
+}
